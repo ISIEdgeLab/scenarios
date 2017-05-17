4efaecc5953c619028125016ff998193925c4887
gbartlet <bartlett@isi.edu>
2017-05-17 10:31:24 -0700

    .

---
+++ 6e-t1-s1/run_scenario.sh
@@ -0,0 +1,19 @@
+source ../scenario_functions.sh
+
+MODULES="general_viz click rtp"
+echo "Starting: ${MODULES}"
+magi_run 6e-t1-s1 "${MODULES}" start
+
+echo "Sleeping"
+sleep 300
+
+magi_run 6e-t1-s1 "click" start_flap
+
+sleep 300
+
+magi_run 6e-t1-s1 "${MODULES}" stop
+
+
+
+
+
