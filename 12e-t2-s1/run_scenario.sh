e2d799f8aea30d7940943eeddcf7e7f74a6d2b18
gbartlet <bartlett@isi.edu>
2017-08-14 11:12:46 -0700

    Added deter dash viz hooks.

--- 12e-t2-s1/run_scenario.sh
+++ 12e-t2-s1/run_scenario.sh
@@ -6,6 +6,9 @@ TRAFFIC="iperf targeted_loss simple_reorder"
 # Available impairments: targeted_loss simple_reorder
 IMPAIRMENT="targeted_loss simple_reorder"
 
+echo "Starting hooks for visulization. See https://edgect.deterlab.net/wiki/VisualizationHowTo"
+magi_run 12e-t2-s1 "general_viz" start
+
 echo "Starting: ${TRAFFIC}"
 magi_run 12e-t2-s1 "${TRAFFIC}" start
 
@@ -20,3 +23,4 @@ sleep 60
 
 magi_run 12e-t2-s1 "${IMPAIRMENT}" stop
 magi_run 12e-t2-s1 "${TRAFFIC}" stop
+magi_run 12e-t2-s1 "general_viz" stop
